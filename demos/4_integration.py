517e37d10ce22e8ad2be3c18b6167a5c32d2ef1b
Dawid Pura <puradawid@gmail.com>
2018-04-02 17:28:53 +0200

    Add demo presentation

--- demos/4_integration.py
+++ demos/4_integration.py
@@ -1,7 +1,21 @@
 # How to integrate with Python environment and VIM?
 
-# Is is worth to do or not?
-
-# What are caveats?
-
 # Is it possible to handle strongly-typed languages as well?
+
+# First thing - debug program!
+
+# Refactor?
+
+
+class OtherClass:
+
+    def whatever(self, n):
+        if n == 0:
+            return 0
+        anotherValue = n + 2
+        return self.whatever(n-1) + anotherValue
+
+
+OtherClass().whatever(2)
+
+print OtherClass().whatever(100)
